6d38af0de87f54f4346016294b7d60ef853895ad
Daniele Giuliani <danielegiuliani0@gmail.com>
2019-11-12 18:05:03 +0100

    Add initial class attributes and method signatures

--- src/lpbcast/Message.java
+++ src/lpbcast/Message.java
@@ -5,8 +5,13 @@ package lpbcast;
 
 /**
  * @author zanel
+ * @author danie
  *
  */
 public abstract class Message {
 	
+	public enum Type {GOSSIP, RETRIEVE_REQUEST, RETRIEVE_REPLY}
+	
+	public int tick;
+	public Type type;
 }
--- src/lpbcast/MissingEvent.java
+++ src/lpbcast/MissingEvent.java
@@ -5,8 +5,12 @@ package lpbcast;
 
 /**
  * @author zanel
+ * @author danie
  *
  */
 public class MissingEvent {
-
+	
+	public EventId eventId;
+	public int tick;
+	public int sender;
 }
